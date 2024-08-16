6323c5666e35539b9050f808827bc79b40372417
Eswar Nadh <eswarnadhch@gmail.com>
2024-08-16 19:29:35 +0530

    new changes

--- App.js
+++ App.js
@@ -1,44 +1,13 @@
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    [
-        React.createElement(
-            "div",
-            {id:"child"},
-            [
-                React.createElement(
-                    "h1",
-                    {},
-                    "I am H1"
-                ),
-                React.createElement(
-                    "h2",
-                    {},
-                    "I am H2"
-                )
-            ]
-    
-        ),
-        React.createElement(
-            "div",
-            {id:"child2"},
-            [
-                React.createElement(
-                    "h1",
-                    {},
-                    "I am H1"
-                ),
-                React.createElement(
-                    "h2",
-                    {},
-                    "I am H2"
-                )
-            ]
-    
-        )
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-    ]
-)
+const Parent =() =>(
+<div id="parent">
+    <div id="child">
+        <h1>Namaste React</h1>
+    </div>
+</div>
+);
 
-const root= ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Parent/>);
---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+#parcel :
+- Dev build
+- local server
+- HMR:(Hot Module Replacement)
+- File watching algorithm
+- Parcel uses caching and cache things for our project for faster build
+- Remove-Item -Recurse -Force .parcel-cache => (command used to remove the cache of the parcel)
+- Image optimization
+- Bundle all the things,Compress the files 
+- minify 
+- Tree Shaking (Removing the dead code and unused code )
